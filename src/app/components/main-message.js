25fc10c9fb095b063fa9966dfd8c968652344d0b
anton <kamaeff2@gmail.com>
2024-03-11 01:14:28 +0300

    add bio link

--- src/app/components/main-message.js
+++ src/app/components/main-message.js
@@ -2,8 +2,9 @@ async function mainMessage(bot, chat_id, username, message_id) {
   bot.sendMessage(
     chat_id,
     `<b>✌🏼 Yo <i>${username}</i></b>! Я помогу тебе подобрать кроссовки по твоему запросу на маркетплейсе <i><a href="https://www.basketshop.ru/">Basketshop</a></i>.\n\n` +
-    `<i><b>🔗 Ссылка на GitHub: </b><a href="https://github.com/kamaeff/searchbottg">Searchbottg</a></i>\n\n` +
-    `<i>💭 Давай для начала выберем твой пол.</i>`,
+      `<i><b>🔗 Ссылка на GitHub: </b><a href="https://github.com/kamaeff/searchbottg">Searchbottg</a></i>\n` +
+      `<i><b>🔗 Ссылка на сайт создателя: </b><a href="http://94.228.124.88:3000/">KamaeffPage</a></i>\n\n` +
+      `<i>💭 Давай для начала выберем твой пол.</i>`,
     {
       parse_mode: 'HTML',
       reply_markup: JSON.stringify({
@@ -11,12 +12,12 @@ async function mainMessage(bot, chat_id, username, message_id) {
           [
             {
               text: '🏀 Basketshop',
-              web_app: { url: 'https://www.basketshop.ru/' },
+              web_app: {url: 'https://www.basketshop.ru/'},
             },
           ],
           [
-            { text: '👨🏼Мужчина', callback_data: 'man' },
-            { text: '👩🏼‍🦰 Женщина', callback_data: 'woman' },
+            {text: '👨🏼Мужчина', callback_data: 'man'},
+            {text: '👩🏼‍🦰 Женщина', callback_data: 'woman'},
           ],
         ],
       }),
